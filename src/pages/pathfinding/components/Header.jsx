25ae240f859284e28614c0864c831bd98c61c4bc
Luan Truong <truongluan303@gmail.com>
2025-04-09 00:03:49 -0700

    Remove unused files

--- src/pages/pathfinding/components/Header.jsx
+++ src/pages/pathfinding/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './Header.css'
 import Dropdown from 'components/Dropdown'
 import Button from 'components/Button'
 import PATHFINDING_OPTIONS from 'pages/pathfinding/pathfindingOptions'
